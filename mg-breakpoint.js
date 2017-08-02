5ae26a0ae345698efae63fb132e229243de61543
Blake Watson <blake@blakewatson.com>
2017-08-02 18:26:16 -0500

    Initial commit

---
+++ mg-breakpoint.js
@@ -0,0 +1,152 @@
+function MGBreakpoint(points, clean) {
+	this.points = this.mergePoints(points, clean);
+	this.lastBreakpoint = this.getCurrentBreakpoint();
+	this.events = this.buildEvents();
+	this.init();
+}
+
+MGBreakpoint.prototype.init = function() {
+	var self = this;
+
+	window.addEventListener('resize', function() {
+		var current = self.getCurrentBreakpoint();
+
+		// if breakpoint has changed
+		if(current.name !== self.lastBreakpoint.name) {
+			// dispatch on for current
+			window.dispatchEvent(self.events[current.name]['on']);
+			// dispatch off for last breakpoint
+			window.dispatchEvent(self.events[self.lastBreakpoint.name]['off']);
+			// update last breakpoint
+			self.lastBreakpoint = current;
+		}
+	});
+};
+
+MGBreakpoint.prototype.mergePoints = function(points, clean) {
+	var defaults = {
+		phone: 0,
+		tabletPortrait: 600,
+		tabletLandscape: 900,
+		desktop: 1200,
+		bigDesktop: 1800
+	};
+	var pointsArray = [];
+
+	// if clean is true, clear out the defaults
+	if(clean) {
+		defaults = {};
+	}
+
+	// merge passed in points with defaults
+	for(var property in points) {
+		if(points.hasOwnProperty(property)) {
+			defaults[property] = points[property];
+		}
+	}
+
+	// create an array of breakpoint objects
+	for(var property in defaults) {
+		if(defaults.hasOwnProperty(property)) {
+			pointsArray.push({
+				name: property,
+				value: defaults[property]
+			});
+		}
+	}
+
+	// sort by breakpoint value
+	pointsArray.sort(function(a, b) {
+		if(a.value < b.value) {
+			return -1;
+		} else if(b.value < a.value) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+
+	return pointsArray;
+};
+
+MGBreakpoint.prototype.on = function(breakpoint, callback) {
+	window.addEventListener(breakpoint.toLowerCase() + 'on', callback);
+};
+
+MGBreakpoint.prototype.off = function(breakpoint, callback) {
+	window.addEventListener(breakpoint.toLowerCase() + 'off', callback);
+};
+
+MGBreakpoint.prototype.min = function(breakpoint, callback) {
+	var result = window.matchMedia('(min-width: ' + this.get(breakpoint).value + 'px)').matches;
+
+	if(callback) {
+		callback();
+	}
+
+	return result;
+};
+
+MGBreakpoint.prototype.max = function(breakpoint, callback) {
+	var result = window.matchMedia('(max-width: ' + this.get(breakpoint).value - 1 + 'px)').matches;
+
+	if(callback) {
+		callback();
+	}
+	
+	return result;
+};
+
+MGBreakpoint.prototype.minmax = function(min, max, callback) {
+	var result = window.matchMedia('(min-width: ' + this.get(min).value + 'px) and (max-width: ' + this.get(max).value - 1 + 'px)').matches;
+
+	if(callback) {
+		callback();
+	}
+	
+	return result;
+};
+
+MGBreakpoint.prototype.get = function(breakpoint) {
+	var result = this.points.filter(function(point) {
+		return point.name === breakpoint;
+	});
+
+
+	return result[0];
+};
+
+MGBreakpoint.prototype.getCurrentBreakpoint = function() {
+	var points = this.points;
+
+	var current = points.filter(function(point, index) {
+		var next = false;
+		var query = '';
+
+		if(index < points.length - 1) {
+			next = points[index + 1];
+		}
+
+		if(next) {
+			query = '(min-width: ' + point.value + 'px) and (max-width: ' + next.value + 'px)';
+		} else {
+			query = '(min-width: ' + point.value + 'px)';
+		}
+
+		return window.matchMedia(query).matches;
+	});
+
+	return current[0];
+};
+
+MGBreakpoint.prototype.buildEvents = function() {
+	var events = {};
+	this.points.forEach(function(point) {
+		var eventOn = new Event(point.name.toLowerCase() + 'on');
+		var eventOff = new Event(point.name.toLowerCase() + 'off');
+		events[point.name] = {};
+		events[point.name]['on'] = eventOn;
+		events[point.name]['off'] = eventOff;
+	});
+	return events;
+};
---
+++ readme.md
@@ -0,0 +1,138 @@
+# MG Breakpoint
+
+MG Breakpoint gives you an easy way to run JavaScript code at different breakpoints. It uses CSS Media Queries by way of the built-in `window.matchMedia()` function and custom events.
+
+## Usage
+
+First, you need to create a new instance:
+
+```js
+var breakpoints = new MGBreakpoint();
+```
+
+By default, MG Breakpoint includes [these 100% correct breakpoint sizes](https://medium.freecodecamp.org/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862):
+
+```
+phone: 0
+tabletPortrait: 600
+tabletLandscape: 900
+desktop: 1200
+bigDesktop: 1800
+```
+
+You can use _these_ breakpoints or your own—more on that later. Now you can run a function when you enter (or leave) one of these breakpoints.
+
+```js
+breakpoints.on('phone', function() {
+	// This code will run as soon as the window goes
+	// from 600px to 599px.
+});
+
+breakpoints.off('desktop', function() {
+	// This code will run as soon as the window goes
+	// up to 1800px or down to 1199px.
+});
+```
+
+## Helpers
+
+ In addition to the on/off events system, you have access to some helper functions.
+
+### min
+
+```js
+// returns true if (min-width: 600px), otherwise returns false.
+breakpoints.min('tabletPortrait');
+
+// You can go ahead and run a function on true if you want
+breakpoints.min('tabletPortrait', function() {
+	// This code runs if (min-width: 600px)
+});
+```
+
+### max
+
+```js
+// returns true if (max-width: 599px), otherwise returns false.
+breakpoints.max('tabletPortrait');
+
+// You can go ahead and run a function on true if you want
+breakpoints.max('tabletPortrait', function() {
+	// This code runs if (max-width: 599px)
+});
+```
+
+### minmax
+
+This helper lets you test for a specific breakpoint range.
+
+```js
+// returns true if (min-width: 600px) and (max-width: 1199px),
+// otherwise returns false.
+breakpoints.minmax('tabletPortrait', 'desktop');
+
+// You can go ahead and run a function on true if you want
+breakpoints.minmax('tabletPortrait', 'desktop', function() {
+	// This code runs if (min-width: 600px) and (max-width: 1199px)
+});
+```
+
+### getCurrentBreakpoint
+
+This helper will return the current breakpoint as defined by `min-width`.
+
+```js
+var currentBP = breakpoints.getCurrentBreakpoint();
+
+console.log(currentBP);
+```
+
+If the window was currently 1400px wide, then this would result in the following object being logged:
+
+```js
+{
+	name: 'desktop',
+	value: 1200
+}
+```
+
+## Custom breakpoints
+
+`MGBreakpoint` accepts two optional arguments in its constructor:
+
+**points**: an object representing breakpoints in terms of `min-width`
+**clean**: a boolean to denote whether you want to clear out the default sizes
+
+Let's say you like the defaults but you just need to add one more specific size, you can do:
+
+```js
+var breakpoints = new MGBreakpoint({
+	yuge: 2400
+});
+```
+
+This will keep the defaults and just add this new breakpoint to the mix.
+
+You can also modify any of the default sizes. So you could do:
+
+```js
+var breakpoints = new MGBreakpoint({
+	bigDesktop: 1500,
+	yuge: 2400
+});
+```
+
+This would both modify `bigDesktop` and add `yuge`—and keep the other defaults intact.
+
+But maybe you have your own breakpoints and you want to scrap the defaults. In that case, pass true as the second argument:
+
+```js
+var breakpoints = new MGBreakpoint({
+	hobbit: 400,
+	dwarf: 700,
+	human: 960,
+	troll: 2200
+}, true); // passing true as second argument
+```
+
+This will throw out the defaults and only use the breakpoints you pass in.
